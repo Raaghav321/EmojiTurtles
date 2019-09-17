bd46c45d22d4a03410f3beeb77ff46b68dc1900f
Raaghav <54276252+Raaghav321@users.noreply.github.com>
2019-09-17 08:57:52 -0500

    Emoji Turtles
    Raaghav Awasthi

---
+++ EmojiTurtles.py
@@ -0,0 +1,50 @@
+import turtle
+
+bob=turtle
+bob.bgcolor("red")
+bob.color("yellow")
+bob.begin_fill()
+bob.circle(100)
+bob.end_fill()
+
+import turtle
+
+cindy=turtle
+cindy.bgcolor("green")
+cindy.color("black")
+cindy.penup()
+cindy.goto(40,-10)
+cindy.begin_fill()
+cindy.left(90)
+cindy.forward(140)
+cindy.right(100)
+cindy.pendown()
+cindy.circle(20)
+cindy.end_fill()
+
+import turtle
+louisa=turtle
+louisa.bgcolor("blue")
+louisa.color("black")
+louisa.penup()
+louisa.goto(-38,129)
+louisa.begin_fill()
+louisa.pendown()
+louisa.circle(20)
+louisa.end_fill()
+
+import turtle
+joe=turtle
+joe.bgcolor("black")
+joe.color("black")
+joe.penup()
+joe.goto(2,40)
+joe.begin_fill()
+joe.pendown()
+joe.circle(40)
+louisa.end_fill()
+
+
+
+
+turtle.exitonclick()
